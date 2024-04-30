56bcdab3e137b2d0b354ed9b657af10e7567a66f
Python_Pioneer <89539411+Tonyjr7@users.noreply.github.com>
2024-04-30 09:46:54 +0100

    Update settings.py

--- newsapp_project/settings.py
+++ newsapp_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)@ce!ef6+29ql^y-fq-8c8u&65pqh2lurfi^c0q(&^lt^h=hr2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['news-app-941i.onrender.com']
+ALLOWED_HOSTS = ['wavy-news-7e4dada83991.herokuapp.com']
 
 
 # Application definition
